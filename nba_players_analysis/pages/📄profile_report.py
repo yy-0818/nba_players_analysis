54403d602539d5b69775a7cba672e0d117747a5e
YOU <534764132@qq.com>
2024-03-30 20:22:39 +0800

    refactor: Add default loading data

--- nba_players_analysis/pages/📄profile_report.py
+++ nba_players_analysis/pages/📄profile_report.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+import os
 
 
 st.set_page_config(
@@ -10,20 +11,36 @@ st.set_page_config(
     page_icon = '🏀',
 )
 # Streamlit 应用界面
-st.title('Data Analysis with ydata_profiling')
+st.title('使用ydata_profiling进行数据分析')
 
-# 用户上传 CSV 文件
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-if uploaded_file is not None:
-    # 读取 CSV 文件为 DataFrame
-    df = pd.read_csv(uploaded_file)
-    
-    # 生成数据分析报告
-    profile = ProfileReport(df, title='Pandas Profiling Report', explorative=True)
-    
-    # 将报告显示在 Streamlit 应用中
-    st_profile_report(profile)
+@st.cache_data
+def read_default_data() -> pd.DataFrame:
+    current_script_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_script_path,"..","data", "all_seasons.csv")
+    df = pd.read_csv(file_path)
+    return df
 
-else:
-    st.write("Please upload a CSV file to generate the report.")
+def main():
+    st.sidebar.title('')
+    # 用户上传 CSV 文件
+    uploaded_file = st.sidebar.file_uploader("上传 CSV 文件", type="csv")
+
+    if uploaded_file is not None:
+        # 如果用户上传了文件，则使用该文件创建渲染器
+        df = pd.read_csv(uploaded_file)
+        st.sidebar.success("文件上传成功.")
+        # 生成数据分析报告
+        profile = ProfileReport(df, explorative=True)
+        # 将报告显示在 Streamlit 应用中
+        st_profile_report(profile)
+    else:
+        # 如果用户没有上传文件，则读取默认数据文件
+        df = read_default_data()
+        profile = ProfileReport(df, explorative=True)
+        st_profile_report(profile)
+        
+        
+if __name__ == "__main__":
+    main()
+
